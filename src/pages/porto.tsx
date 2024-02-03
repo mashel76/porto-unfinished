e66b5ef9914e7b57efd0a2fb73d84ad622981345
MasHel <40082911+mashel76@users.noreply.github.com>
2024-02-03 20:35:58 +0700

    adjust

--- src/pages/porto.tsx
+++ src/pages/porto.tsx
@@ -12,9 +12,9 @@ const porto = () => {
       className="py-40 mx-10 text-[14px]"
     >
       <Flicking className="text-center flex flex-col items-center justify-center" circular={true}>
-        <div className="bg-blue-500 w-2/3 h-[97%]">Link Porto 1</div>
-        <div className="bg-red-500 w-96 h-96">Link Porto 2</div>
-        <div className="bg-green-500 w-96 h-96">Link Porto 3</div>
+        <div className="bg-blue-500 w-1/2 h-full]">Link Porto 1</div>
+        <div className="bg-yellow-500 w-1/2 h-full">Link Porto 2</div>
+        <div className="bg-green-500 w-1/2 h-96">Link Porto 3</div>
       </Flicking>
     </motion.div>
   )
